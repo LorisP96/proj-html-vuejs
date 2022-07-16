7b1c780f4ad04043ba183dcb7cf4fca40e90cb66
LorisP96 <lorispalazzo96@gmail.com>
2022-07-16 17:28:28 +0200

    fine main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -143,6 +143,53 @@
             </ul>
         </div>
     </section>
+
+    <!-- DEALS -->
+    <section class="deals specials-container flex">
+        <div class="special">
+            <h6>MADE WITH LOVE</h6>
+            <h2>DELISH PIZZA DEALS</h2>
+            <ul class="special-list">
+                <li class="flex">
+                    <div class="date">
+                        <div class="date-number">02</div>
+                        <div class="date-month">NOV</div>
+                    </div>
+                    <div class="info-deals">
+                        <h3>TRADITIONAL NEAPOLITAN PIES IN KYOTO PIZZA MERCATO</h3>
+                        <h6>204 E. Pizzetta Tommaso</h6>
+                    </div>
+                </li>
+
+                <li class="flex">
+                    <div class="date">
+                        <div class="date-number">03</div>
+                        <div class="date-month">NOV</div>
+                    </div>
+                    <div class="info-deals">
+                        <h3>TERRAZZA PATIO DINING SPACE OPENING THIS WEEKEND</h3>
+                        <h6>204 E. Pizzetta Tommaso</h6>
+                    </div>
+                </li>
+
+                <li class="flex">
+                    <div class="date">
+                        <div class="date-number">05</div>
+                        <div class="date-month">NOV</div>
+                    </div>
+                    <div class="info-deals">
+                        <h3>SIENNA PRIVATE DINING ROOM WITH STEPHAN BRUNN</h3>
+                        <h6>204 E. Pizzetta Tommaso</h6>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="special">
+            <img src="../assets/img/h3-img-5a.jpg" alt="">
+        </div>
+    </section>
+
+    
   </main>
 </template>
 
@@ -421,5 +468,62 @@ export default {
             }
         }
         
+    }
+
+    // DEALS
+    .deals {
+        align-items: flex-start;
+
+        h6 {
+            font-weight: 400;
+            font-size: 10px;
+        }
+        .special:first-child {
+            background-color: $bg-black-1;
+            color: $txt-white-1;
+            padding-block: 110px;
+
+            .special-list {
+                li {
+                    padding-block: 40px;
+                    border-bottom: 1px dotted $border-gold;
+
+                    .date {
+                        text-align: center;
+                        padding-right: 35px;
+
+                        .date-number {
+                            font-size: 40px;
+                            font-weight: bolder;
+                            color: $txt-red-2;
+                            padding-top: 0;
+                            line-height: 40px;
+                            -webkit-text-stroke: 1px $txt-red-2;
+                        }
+                    }
+
+                    &:last-child {
+                            padding-bottom: 0;
+                        }
+
+                    h3 {
+                        font-weight: 500;
+                        font-size: 20px;
+                    }
+
+                    h6 {
+                        padding-top: 8px;
+                        font-weight: 300;
+                    }
+
+                    &:last-child {
+                        border-bottom: none;
+                    }
+                }
+            }
+        }
+        .special:last-child {
+            padding: 0;
+        }
     }
 </style>
